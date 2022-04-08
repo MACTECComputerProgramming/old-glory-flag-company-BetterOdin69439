f7260bf012f5bde8d486c66c921dc66f6bc3fa6e
BetterOdin69439 <70332383+BetterOdin69439@users.noreply.github.com>
2022-04-08 10:14:30 -0500

    Ehhhhhh

--- Old Glory/Old Glory/Models/Customer.cs
+++ Old Glory/Old Glory/Models/Customer.cs
@@ -2,6 +2,8 @@
 {
     public class Customer
     {
+        internal readonly object Title;
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
